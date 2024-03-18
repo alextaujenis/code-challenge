1fadfc6c5d3d9e098090cbdf6ad5d709bf4aad0b
Alex Taujenis <alex.taujenis@gmail.com>
2024-03-18 14:03:57 -0600

    Add test for /common_ancestor

--- spec/controllers/common_ancestor_controller_spec.rb
+++ spec/controllers/common_ancestor_controller_spec.rb
@@ -1,11 +1,15 @@
 describe CommonAncestorController, type: :request do
   describe "GET #index" do
     let (:expected_data) do
-      { hello: "world" }
+      {
+        root_id: 1045177,
+        lowest_common_ancestor: 2138651,
+        depth: 4
+      }
     end
 
-    it "responds with json" do
-      get "/common_ancestor"
+    it "responds with the expected_data" do
+      get "/common_ancestor?a=2138692&b=2138651"
       expect(response.body).to eq(expected_data.to_json)
     end
   end
