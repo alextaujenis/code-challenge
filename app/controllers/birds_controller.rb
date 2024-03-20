98af54770c58742c49e291013e455f822baf66bf
Alex Taujenis <alex.taujenis@gmail.com>
2024-03-20 09:23:25 -0600

    Change ruby uniq to database distinct

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,7 +1,7 @@
 class BirdsController < ApplicationController
   def search
     all_node_ids = Node.all_children_ids(Set.new, Set.new(params.require(:node_ids)))
-    all_bird_ids = Bird.includes(:nodes).where(nodes: { id: all_node_ids }).pluck(:id).uniq
+    all_bird_ids = Bird.includes(:nodes).where(nodes: { id: all_node_ids }).distinct.pluck(:id)
     render json: { bird_ids: all_bird_ids }
   end
 end
